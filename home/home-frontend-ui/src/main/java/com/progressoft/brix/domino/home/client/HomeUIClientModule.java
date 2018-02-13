c0e3c7f241f0833157cb57efab40bb46c1b1c69e
Ahmad Bawaneh <akabme@gmail.com>
2018-02-14 00:48:37 +0200

    add more demo content

--- home/home-frontend-ui/src/main/java/com/progressoft/brix/domino/home/client/HomeUIClientModule.java
+++ home/home-frontend-ui/src/main/java/com/progressoft/brix/domino/home/client/HomeUIClientModule.java
@@ -3,9 +3,6 @@ package com.progressoft.brix.domino.home.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.progressoft.brix.domino.api.client.ModuleConfigurator;
 import com.progressoft.brix.domino.api.client.annotations.ClientModule;
-
-import me.rjb.client.SyntaxHighlighter;
-import me.rjb.client.themes.Themes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- ui-demo-frontend/src/main/java/com/progressoft/brix/domino/ui/demo/AppClientModule.java
+++ ui-demo-frontend/src/main/java/com/progressoft/brix/domino/ui/demo/AppClientModule.java
@@ -2,9 +2,6 @@ package com.progressoft.brix.domino.ui.demo;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.progressoft.brix.domino.api.client.ClientApp;
-import com.google.gwt.user.client.History;
-import me.rjb.client.SyntaxHighlighter;
-import me.rjb.client.themes.Themes;
 
 import java.util.logging.Logger;
 
@@ -13,7 +10,6 @@ public class AppClientModule implements EntryPoint {
     private static final Logger LOGGER = Logger.getLogger(AppClientModule.class.getName());
 
     public void onModuleLoad() {
-        SyntaxHighlighter.init(Themes.Eclipse);
         ClientApp.make().run();
         LOGGER.info("ui-demo Application frontend have been initialized.");
     }
