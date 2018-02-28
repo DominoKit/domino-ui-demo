491875041f58a32ccc093b83e40d32416a762981
Ahmad Bawaneh <akabme@gmail.com>
2018-02-28 12:55:25 +0200

    add animations

---
+++ animation/src/main/java/com/progressoft/brix/domino/animation/shared/extension/AnimationExtensionPoint.java
@@ -0,0 +1,6 @@
+package com.progressoft.brix.domino.animation.shared.extension;
+
+import com.progressoft.brix.domino.api.shared.extension.ExtensionPoint;
+
+public interface AnimationExtensionPoint extends ExtensionPoint<AnimationContext>{
+}
