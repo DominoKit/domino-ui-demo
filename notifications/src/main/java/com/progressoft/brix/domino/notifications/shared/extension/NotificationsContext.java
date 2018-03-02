8b83ec78311ff0b82672e0669ef0974259b90552
Ahmad Bawaneh <akabme@gmail.com>
2018-03-02 02:02:28 +0200

    add notifications

---
+++ notifications/src/main/java/com/progressoft/brix/domino/notifications/shared/extension/NotificationsContext.java
@@ -0,0 +1,7 @@
+package com.progressoft.brix.domino.notifications.shared.extension;
+
+
+import com.progressoft.brix.domino.api.shared.extension.Context;
+
+public interface NotificationsContext extends Context {
+}
---
+++ notifications/src/main/java/com/progressoft/brix/domino/notifications/shared/extension/NotificationsExtensionPoint.java
@@ -0,0 +1,6 @@
+package com.progressoft.brix.domino.notifications.shared.extension;
+
+import com.progressoft.brix.domino.api.shared.extension.ExtensionPoint;
+
+public interface NotificationsExtensionPoint extends ExtensionPoint<NotificationsContext>{
+}
