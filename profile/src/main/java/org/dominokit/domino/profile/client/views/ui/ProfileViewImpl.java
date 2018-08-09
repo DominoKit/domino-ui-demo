86ace39552e9c938189bb6d6b0f84d4143936a3e
rjeeb <rafat.albarouki@gmail.com>
2018-08-09 14:35:20 +0300

    add dropdown on the profile

--- profile/src/main/java/org/dominokit/domino/profile/client/views/ui/ProfileViewImpl.java
+++ profile/src/main/java/org/dominokit/domino/profile/client/views/ui/ProfileViewImpl.java
@@ -1,44 +1,48 @@
 package org.dominokit.domino.profile.client.views.ui;
 
 import com.google.gwt.core.client.GWT;
+import elemental2.dom.CSSProperties;
+import elemental2.dom.HTMLElement;
+import jsinterop.base.Js;
 import org.dominokit.domino.api.client.annotations.UiView;
 import org.dominokit.domino.layout.shared.extension.IsLayout;
 import org.dominokit.domino.profile.client.presenters.ProfilePresenter;
 import org.dominokit.domino.profile.client.views.ProfileView;
+import org.dominokit.domino.ui.button.DropdownAction;
+import org.dominokit.domino.ui.button.DropdownButton;
 import org.dominokit.domino.ui.cards.Card;
-import org.dominokit.domino.ui.icons.Icon;
 import org.dominokit.domino.ui.icons.Icons;
-import elemental2.dom.CSSProperties;
-import elemental2.dom.HTMLElement;
-import elemental2.dom.HTMLLIElement;
-import jsinterop.base.Js;
+import org.dominokit.domino.ui.notifications.Notification;
+import org.dominokit.domino.ui.style.Color;
 import org.jboss.gwt.elemento.core.Elements;
 
 
 @UiView(presentable = ProfilePresenter.class)
 public class ProfileViewImpl implements ProfileView {
 
-    private final Card profile=Card.createProfile("Vegegoku", "vegegoku@bo3.com");
+    private final Card profile = Card.createProfile("Vegegoku", "vegegoku@bo3.com");
 
     public ProfileViewImpl() {
     }
 
     @Override
     public void setLayout(IsLayout layout) {
-        HTMLElement leftPanel= Js.cast(layout.getLeftPanel().get());
-        if(leftPanel.childElementCount>0)
+        HTMLElement leftPanel = Js.cast(layout.getLeftPanel().get());
+        if (leftPanel.childElementCount > 0)
             leftPanel.insertBefore(profile.asElement(), leftPanel.firstChild);
         else
             leftPanel.appendChild(profile.asElement());
 
-        profile.getBody().appendChild(Elements.img(GWT.getModuleBaseURL()+"/images/user.png").style("border-radius:50%;").asElement());
-        profile.getHeaderBar().appendChild(createIcon(Icons.ALL.more_vert()));
-        profile.asElement().style.height= CSSProperties.HeightUnionType.of(300);
-    }
+        profile.getBody().appendChild(Elements.img(GWT.getModuleBaseURL() + "/images/user.png").style("border-radius:50%;").asElement());
+        DropdownButton dropdownButton = DropdownButton.create(Icons.ALL.more_vert())
+                .linkify()
+                .setBackground(Color.TRANSPARENT)
+                .hideCaret()
+                .addAction(DropdownAction.create("Action 1").addSelectionHandler(() -> Notification.createInfo("Action 1").show()))
+                .addAction(DropdownAction.create("Action 2").addSelectionHandler(() -> Notification.createInfo("Action 2").show()));
 
-    private HTMLLIElement createIcon(Icon icon) {
-        return Elements.li().add(
-                Elements.a().add(icon.asElement()))
-                .asElement();
+        profile.getHeaderBar().appendChild(dropdownButton
+                .asElement());
+        profile.asElement().style.height = CSSProperties.HeightUnionType.of(300);
     }
 }
