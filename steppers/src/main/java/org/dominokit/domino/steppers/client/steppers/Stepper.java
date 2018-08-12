c863431b0839aeff731592af17f86c89e2a9340a
Ahmad Bawaneh <akabme@gmail.com>
2018-08-12 09:38:36 +0300

    upate form sample

--- steppers/src/main/java/org/dominokit/domino/steppers/client/steppers/Stepper.java
+++ steppers/src/main/java/org/dominokit/domino/steppers/client/steppers/Stepper.java
@@ -1,14 +1,12 @@
 package org.dominokit.domino.steppers.client.steppers;
 
 import elemental2.dom.HTMLUListElement;
-import org.dominokit.domino.ui.collapsible.Collapsible;
 import org.jboss.gwt.elemento.core.IsElement;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Objects.isNull;
-import static org.jboss.gwt.elemento.core.Elements.select;
 import static org.jboss.gwt.elemento.core.Elements.ul;
 
 public class Stepper implements IsElement<HTMLUListElement> {
@@ -16,7 +14,6 @@ public class Stepper implements IsElement<HTMLUListElement> {
     private final HTMLUListElement element = ul().css("stepper").asElement();
     private Step activeStep;
     private List<Step> steps = new ArrayList<>();
-    private Collapsible.CollapseCompletedHandler collapseCompletedHandler;
 
     public static Stepper create() {
         return new Stepper();
@@ -25,12 +22,7 @@ public class Stepper implements IsElement<HTMLUListElement> {
     public Stepper addStep(Step step) {
         element.appendChild(step.asElement());
         if (isNull(activeStep)) {
-            collapseCompletedHandler = () -> {
-                step.activate();
-                step.collapsible.removeCollapseHandler(collapseCompletedHandler);
-            };
-            step.collapsible.addCollapseHandler(collapseCompletedHandler);
-
+            step.activate();
             this.activeStep = step;
         }
         steps.add(step);
@@ -43,8 +35,8 @@ public class Stepper implements IsElement<HTMLUListElement> {
         return element;
     }
 
-    public Stepper activateStep(Step step){
-        if(steps.contains(step)){
+    public Stepper activateStep(Step step) {
+        if (steps.contains(step)) {
             this.activeStep.deActivate();
             step.activate();
             step.setDone(false);
