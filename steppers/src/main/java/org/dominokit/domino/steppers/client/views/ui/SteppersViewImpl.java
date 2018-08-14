02761e46cd7817acfc90a08167a0e65d598cfad8
Ahmad Bawaneh <akabme@gmail.com>
2018-08-14 15:46:13 +0300

    implement steppers https://github.com/DominoKit/domino-ui/issues/70

--- steppers/src/main/java/org/dominokit/domino/steppers/client/views/ui/SteppersViewImpl.java
+++ steppers/src/main/java/org/dominokit/domino/steppers/client/views/ui/SteppersViewImpl.java
@@ -10,9 +10,6 @@ import org.dominokit.domino.steppers.client.views.SteppersView;
 import org.dominokit.domino.ui.alerts.Alert;
 import org.dominokit.domino.ui.button.Button;
 import org.dominokit.domino.ui.cards.Card;
-import org.dominokit.domino.ui.forms.FieldsGrouping;
-import org.dominokit.domino.ui.forms.Select;
-import org.dominokit.domino.ui.forms.SelectOption;
 import org.dominokit.domino.ui.forms.TextBox;
 import org.dominokit.domino.ui.header.BlockHeader;
 import org.dominokit.domino.ui.icons.Icons;
@@ -35,115 +32,48 @@ public class SteppersViewImpl extends ComponentView<HTMLDivElement> implements S
         element.appendChild(Alert.warning().appendStrong("Warning: ").appendText("This is still a work in progress").asElement());
         element.appendChild(BlockHeader.create("STEPPERS").asElement());
 
+        verticalStepper();
+
+        horizontalStepper();
+
+    }
+
+    private void verticalStepper() {
         Stepper stepper = Stepper.create();
 
-        FieldsGrouping stepOneFieldsGrouping = FieldsGrouping.create();
-        FieldsGrouping stepTowFieldsGrouping = FieldsGrouping.create();
-        FieldsGrouping stepThreeFieldsGrouping = FieldsGrouping.create();
-
-        TextBox firstNameTextBox = TextBox.create("First name")
+        TextBox nameTextBox = TextBox.create("Name")
                 .setRequired(true)
                 .setAutoValidation(true)
-                .setLeftAddon(Icons.ALL.label())
-                .groupBy(stepOneFieldsGrouping);
+                .setLeftAddon(Icons.ALL.label());
 
-        TextBox lastNameTextBox = TextBox.create("Last name")
+        TextBox email = TextBox.create("Email")
                 .setRequired(true)
                 .setAutoValidation(true)
-                .setLeftAddon(Icons.ALL.label())
-                .groupBy(stepOneFieldsGrouping);
+                .setLeftAddon(Icons.ALL.email());
 
-        TextBox emailTextBox = TextBox.create("Email")
+        TextBox phone = ElementUtil.numbersOnly(TextBox.create("Phone"))
                 .setRequired(true)
                 .setAutoValidation(true)
-                .setLeftAddon(Icons.ALL.email())
-                .groupBy(stepOneFieldsGrouping);
-
-        TextBox phoneNumberTextBox = ElementUtil.numbersOnly(TextBox.create("Phone number"))
-                .setRequired(true)
-                .setAutoValidation(true)
-                .setLeftAddon(Icons.ALL.phone())
-                .groupBy(stepOneFieldsGrouping);
-
-        TextBox addressLine1TextBox = TextBox.create("Address line 1")
-                .setRequired(true)
-                .setAutoValidation(true)
-                .setLeftAddon(Icons.ALL.location_on())
-                .groupBy(stepTowFieldsGrouping);
-
-        TextBox addressLine2TextBox = TextBox.create("Address line 2")
-                .setRequired(true)
-                .setAutoValidation(true)
-                .setLeftAddon(Icons.ALL.location_on())
-                .groupBy(stepTowFieldsGrouping);
-
-        TextBox cityTextBox = TextBox.create("City")
-                .setRequired(true)
-                .setAutoValidation(true)
-                .setLeftAddon(Icons.ALL.location_city())
-                .groupBy(stepTowFieldsGrouping);
-
-        TextBox stateTextBox = TextBox.create("State / Province")
-                .setRequired(true)
-                .setAutoValidation(true)
-                .setLeftAddon(Icons.ALL.location_city())
-                .groupBy(stepTowFieldsGrouping);
-
-        TextBox postalCodeTextBox = TextBox.create("Postal / Zip code")
-                .setRequired(true)
-                .setAutoValidation(true)
-                .setLeftAddon(Icons.ALL.label())
-                .groupBy(stepTowFieldsGrouping);
-
-        Select<String> countrySelect = Select.<String>create("Country")
-                .setRequired(true)
-                .setAutoValidation(true)
-                .setLeftAddon(Icons.ALL.location_city())
-                .groupBy(stepTowFieldsGrouping)
-                .addOption(SelectOption.create("Jordan", "Jordan"))
-                .addOption(SelectOption.create("Qatar", "Qatar"))
-                .addOption(SelectOption.create("Italy", "Italy"));
-
-        TextBox organizationTextBox = TextBox.create("Organization")
-                .setRequired(true)
-                .setAutoValidation(true)
-                .setLeftAddon(Icons.ALL.domain())
-                .groupBy(stepThreeFieldsGrouping);
-
-        TextBox jobTitleTextBox = TextBox.create("Job title")
-                .setRequired(true)
-                .setAutoValidation(true)
-                .setLeftAddon(Icons.ALL.domain())
-                .groupBy(stepThreeFieldsGrouping);
+                .setLeftAddon(Icons.ALL.phone());
 
 
-        HTMLDivElement stepOneContainer = div()
-                .add(Row.create()
-                        .addColumn(span6().addElement(firstNameTextBox))
-                        .addColumn(span6().addElement(lastNameTextBox))
-                ).add(Row.create()
-                        .addColumn(span6().addElement(emailTextBox))
-                        .addColumn(span6().addElement(phoneNumberTextBox))
-                ).add(Row.create()
+        Step stepOne = Step.create("Contact name", "Contact name step")
+                .setValidator(() -> nameTextBox.validate().isValid())
+                .appendChild(Row.create()
+                        .addColumn(span6().addElement(nameTextBox)))
+                .appendFooterChild(Row.create()
                         .addColumn(span12().addElement(Style.of(Button.createPrimary("Next"))
                                 .setMinWidth("120px")
                                 .setMarginRight("5px")
                                 .get()
                                 .addClickListener(evt -> stepper.next())
-                        )))
-                .asElement();
+                        )));
 
-        HTMLDivElement stepTowContainer = div()
-                .add(Row.create()
-                        .addColumn(span12().addElement(addressLine1TextBox))
-                        .addColumn(span12().addElement(addressLine2TextBox))
-                ).add(Row.create()
-                        .addColumn(span6().addElement(cityTextBox))
-                        .addColumn(span6().addElement(stateTextBox))
-                ).add(Row.create()
-                        .addColumn(span6().addElement(postalCodeTextBox))
-                        .addColumn(span6().addElement(countrySelect))
-                ).add(Row.create()
+        Step stepTow = Step.create("Contact email", "Contact email step")
+                .setValidator(() -> email.validate().isValid())
+                .appendChild(Row.create()
+                        .addColumn(span12().addElement(email)))
+                .appendFooterChild(Row.create()
                         .addColumn(span12()
                                 .addElement(Style.of(Button.createPrimary("Back"))
                                         .setMinWidth("120px")
@@ -155,15 +85,13 @@ public class SteppersViewImpl extends ComponentView<HTMLDivElement> implements S
                                         .setMarginRight("5px")
                                         .get()
                                         .addClickListener(evt -> stepper.next())
-                                )))
-                .asElement();
+                                )));
 
-
-        HTMLDivElement stepThreeContainer = div()
-                .add(Row.create()
-                        .addColumn(span6().addElement(organizationTextBox))
-                        .addColumn(span6().addElement(jobTitleTextBox))
-                ).add(Row.create()
+        Step stepThree = Step.create("Contact phone", "Contact phone step")
+                .setValidator(() -> phone.validate().isValid())
+                .appendChild(Row.create()
+                        .addColumn(span6().addElement(phone)))
+                .appendFooterChild(Row.create()
                         .addColumn(span12()
                                 .addElement(Style.of(Button.createPrimary("Back"))
                                         .setMinWidth("120px")
@@ -177,31 +105,103 @@ public class SteppersViewImpl extends ComponentView<HTMLDivElement> implements S
                                         .setMarginRight("5px")
                                         .get()
                                         .addClickListener(evt -> stepper.finish())
-                                )))
-                .asElement();
-
-
-        Step stepOne = Step.create("Contact info", "Contact basic information")
-                .appendChild(stepOneContainer)
-                .setValidator(() -> stepOneFieldsGrouping.validate().isValid());
-        Step stepTow = Step.create("Address info", "Contact address information")
-                .appendChild(stepTowContainer)
-                .setValidator(() -> stepTowFieldsGrouping.validate().isValid());
-        Step stepThree = Step.create("Organization", "Contact organization and job title")
-                .appendChild(stepThreeContainer)
-                .setValidator(() -> stepThreeFieldsGrouping.validate().isValid());
+                                )));
 
 
         stepper
                 .addStep(stepOne)
                 .addStep(stepTow)
                 .addStep(stepThree)
-        .setCompletionHandler(() -> Notification.create("All step completed").show());
-        element.appendChild(Card.create()
-                .appendChild(Row.create()
-                        .addColumn(span6().addElement(stepper)))
-                .asElement());
+                .setCompletionHandler(() -> Notification.create("All step completed").show());
 
+        element.appendChild(Card.create("VERTICAL STEPPER")
+                .appendChild(Row.create()
+                        .addColumn(span12().addElement(stepper)))
+                .asElement());
+    }
+
+    private void horizontalStepper() {
+        Stepper stepper = Stepper.create()
+                .setMinHeight("300px")
+                .setHorizontal(true);
+
+        TextBox nameTextBox = TextBox.create("Name")
+                .setRequired(true)
+                .setAutoValidation(true)
+                .setLeftAddon(Icons.ALL.label());
+
+        TextBox email = TextBox.create("Email")
+                .setRequired(true)
+                .setAutoValidation(true)
+                .setLeftAddon(Icons.ALL.email());
+
+        TextBox phone = ElementUtil.numbersOnly(TextBox.create("Phone"))
+                .setRequired(true)
+                .setAutoValidation(true)
+                .setLeftAddon(Icons.ALL.phone());
+
+
+        Step stepOne = Step.create("Contact name", "Contact name step")
+                .setValidator(() -> nameTextBox.validate().isValid())
+                .appendChild(Row.create()
+                        .addColumn(span6().addElement(nameTextBox)))
+                .appendFooterChild(Row.create()
+                        .addColumn(span12().addElement(Style.of(Button.createPrimary("Next"))
+                                .setMinWidth("120px")
+                                .setMarginRight("5px")
+                                .get()
+                                .addClickListener(evt -> stepper.next())
+                        )));
+
+        Step stepTow = Step.create("Contact email", "Contact email step")
+                .setValidator(() -> email.validate().isValid())
+                .appendChild(Row.create()
+                        .addColumn(span12().addElement(email)))
+                .appendFooterChild(Row.create()
+                        .addColumn(span12()
+                                .addElement(Style.of(Button.createPrimary("Back"))
+                                        .setMinWidth("120px")
+                                        .setMarginRight("5px")
+                                        .get()
+                                        .addClickListener(evt -> stepper.back()))
+                                .addElement(Style.of(Button.createPrimary("Next"))
+                                        .setMinWidth("120px")
+                                        .setMarginRight("5px")
+                                        .get()
+                                        .addClickListener(evt -> stepper.next())
+                                )));
+
+        Step stepThree = Step.create("Contact phone", "Contact phone step")
+                .setValidator(() -> phone.validate().isValid())
+                .appendChild(Row.create()
+                        .addColumn(span6().addElement(phone)))
+                .appendFooterChild(Row.create()
+                        .addColumn(span12()
+                                .addElement(Style.of(Button.createPrimary("Back"))
+                                        .setMinWidth("120px")
+                                        .setMarginRight("5px")
+                                        .get()
+                                        .addClickListener(evt -> {
+                                            stepper.back();
+                                        }))
+                                .addElement(Style.of(Button.createPrimary("Finish"))
+                                        .setMinWidth("120px")
+                                        .setMarginRight("5px")
+                                        .get()
+                                        .addClickListener(evt -> stepper.finish())
+                                )));
+
+
+        stepper
+                .addStep(stepOne)
+                .addStep(stepTow)
+                .addStep(stepThree)
+                .setCompletionHandler(() -> Notification.create("All step completed").show());
+
+        element.appendChild(Card.create("HORIZONTAL STEPPER")
+                .appendChild(Row.create()
+                        .addColumn(span12().addElement(stepper)))
+                .asElement());
     }
 
     @Override
