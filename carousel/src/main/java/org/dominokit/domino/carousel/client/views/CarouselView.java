e8161824850468578b4c7aa0c6fc41af48d342a1
Ahmad Bawaneh <akabme@gmail.com>
2018-08-15 22:10:34 +0300

    implement carousel

---
+++ carousel/src/main/java/org/dominokit/domino/carousel/client/views/CarouselView.java
@@ -0,0 +1,7 @@
+package org.dominokit.domino.carousel.client.views;
+
+import org.dominokit.domino.api.client.mvp.view.View;
+import org.dominokit.domino.componentcase.shared.extension.DemoView;
+
+public interface CarouselView extends View, DemoView {
+}
