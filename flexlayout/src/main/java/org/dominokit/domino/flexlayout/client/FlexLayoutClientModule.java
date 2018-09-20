8e2fd707560ef4126845f061288ec57b252802fe
rjeeb <rafat.albarouki@gmail.com>
2018-09-20 14:10:00 +0300

    Add FlexLayout sample

---
+++ flexlayout/src/main/java/org/dominokit/domino/flexlayout/client/FlexLayoutClientModule.java
@@ -0,0 +1,19 @@
+package org.dominokit.domino.flexlayout.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import org.dominokit.domino.api.client.ModuleConfigurator;
+import org.dominokit.domino.api.client.annotations.ClientModule;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ClientModule(name="FlexLayout")
+public class FlexLayoutClientModule implements EntryPoint {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(FlexLayoutClientModule.class);
+
+	public void onModuleLoad() {
+		LOGGER.info("Initializing FlexLayout frontend module ...");
+		new ModuleConfigurator().configureModule(new FlexLayoutModuleConfiguration());
+	}
+}
