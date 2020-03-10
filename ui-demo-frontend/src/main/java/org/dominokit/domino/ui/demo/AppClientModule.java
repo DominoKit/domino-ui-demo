5b8de10ea063c470928ea21dcfea6f9af19d481e
Ahmad bawaneh <akabme@gmail.com>
2020-03-10 12:37:46 +0200

    update to latest domino-ui

--- ui-demo-frontend/src/main/java/org/dominokit/domino/ui/demo/AppClientModule.java
+++ ui-demo-frontend/src/main/java/org/dominokit/domino/ui/demo/AppClientModule.java
@@ -2,6 +2,8 @@ package org.dominokit.domino.ui.demo;
 
 import com.google.gwt.core.client.EntryPoint;
 import org.dominokit.domino.api.client.ClientApp;
+import org.dominokit.domino.gwt.client.app.DominoGWT;
+import org.dominokit.domino.view.GwtView;
 
 import java.util.logging.Logger;
 
@@ -10,6 +12,8 @@ public class AppClientModule implements EntryPoint {
     private static final Logger LOGGER = Logger.getLogger(AppClientModule.class.getName());
 
     public void onModuleLoad() {
+        DominoGWT.init();
+        GwtView.init();
         ClientApp.make().run();
         LOGGER.info("ui-demo Application frontend have been initialized.");
     }
