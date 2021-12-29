10ec0d6a8a9bea4636fcf32e56c21dd2a0bc9efe
Ahmad K. Bawaneh <ahmad.bawaneh@dominokit.com>
2021-12-29 22:13:53 +0200

    Implementing Tree grid (#18)
    * implementing tree grid
    Co-authored-by: rjeeb <rafat.albarouki@gmail.com>

--- datatable/src/main/java/org/dominokit/domino/datatable/client/views/ui/DataTableViewImpl.java
+++ datatable/src/main/java/org/dominokit/domino/datatable/client/views/ui/DataTableViewImpl.java
@@ -6,6 +6,7 @@ import com.google.gwt.resources.client.TextResource;
 import elemental2.dom.DomGlobal;
 import elemental2.dom.HTMLDivElement;
 import elemental2.dom.HTMLElement;
+import elemental2.dom.HTMLTableCellElement;
 import org.dominokit.domino.SampleClass;
 import org.dominokit.domino.SampleMethod;
 import org.dominokit.domino.api.client.annotations.UiView;
@@ -21,10 +22,14 @@ import org.dominokit.domino.datatable.client.views.model.ContactSearchFilter;
 import org.dominokit.domino.datatable.client.views.model.ContactSorter;
 import org.dominokit.domino.datatable.client.views.model.EyeColor;
 import org.dominokit.domino.datatable.client.views.model.Gender;
+import org.dominokit.domino.datatable.client.views.model.TreeGridSample;
+import org.dominokit.domino.ui.Typography.Paragraph;
 import org.dominokit.domino.ui.badges.Badge;
 import org.dominokit.domino.ui.cards.Card;
+import org.dominokit.domino.ui.datatable.CellRenderer;
 import org.dominokit.domino.ui.datatable.ColumnConfig;
 import org.dominokit.domino.ui.datatable.DataTable;
+import org.dominokit.domino.ui.datatable.RowCell;
 import org.dominokit.domino.ui.datatable.TableConfig;
 import org.dominokit.domino.ui.datatable.events.TableDataUpdatedEvent;
 import org.dominokit.domino.ui.datatable.events.TableEvent;
@@ -34,12 +39,15 @@ import org.dominokit.domino.ui.datatable.plugins.ColumnHeaderFilterPlugin;
 import org.dominokit.domino.ui.datatable.plugins.GroupingPlugin;
 import org.dominokit.domino.ui.datatable.plugins.HeaderBarPlugin;
 import org.dominokit.domino.ui.datatable.plugins.RecordDetailsPlugin;
+import org.dominokit.domino.ui.datatable.plugins.RowClickPlugin;
 import org.dominokit.domino.ui.datatable.plugins.RowMarkerPlugin;
 import org.dominokit.domino.ui.datatable.plugins.ScrollingPaginationPlugin;
 import org.dominokit.domino.ui.datatable.plugins.SelectionPlugin;
 import org.dominokit.domino.ui.datatable.plugins.SimplePaginationPlugin;
+import org.dominokit.domino.ui.datatable.plugins.SortDirection;
 import org.dominokit.domino.ui.datatable.plugins.SortPlugin;
 import org.dominokit.domino.ui.datatable.plugins.TopPanelPlugin;
+import org.dominokit.domino.ui.datatable.plugins.TreeGridPlugin;
 import org.dominokit.domino.ui.datatable.plugins.filter.header.BooleanHeaderFilter;
 import org.dominokit.domino.ui.datatable.plugins.filter.header.DoubleHeaderFilter;
 import org.dominokit.domino.ui.datatable.plugins.filter.header.EnumHeaderFilter;
@@ -55,6 +63,8 @@ import org.dominokit.domino.ui.forms.Select;
 import org.dominokit.domino.ui.forms.SelectOption;
 import org.dominokit.domino.ui.forms.TelephoneBox;
 import org.dominokit.domino.ui.forms.TextBox;
+import org.dominokit.domino.ui.grid.Column;
+import org.dominokit.domino.ui.grid.Row;
 import org.dominokit.domino.ui.header.BlockHeader;
 import org.dominokit.domino.ui.icons.Icon;
 import org.dominokit.domino.ui.icons.Icons;
@@ -66,12 +76,19 @@ import org.dominokit.domino.ui.utils.DominoElement;
 import org.dominokit.domino.ui.utils.TextNode;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 import static org.dominokit.domino.ui.style.Unit.px;
 import static org.jboss.elemento.Elements.a;
 import static org.jboss.elemento.Elements.div;
+import static org.jboss.elemento.Elements.fieldset;
+import static org.jboss.elemento.Elements.td;
 
 @UiView(presentable = DatatableProxy.class)
 @SampleClass
@@ -137,8 +154,15 @@ public class DataTableViewImpl extends BaseDemoView<HTMLDivElement> implements D
         groupingTable();
         element.appendChild(CodeCard.createCodeCard(CodeResource.INSTANCE.groupingTable()).element());
 
+        treeGridFullParentSpan();
+        element.appendChild(CodeCard.createCodeCard(CodeResource.INSTANCE.treeGridFullParentSpan()).element());
+
+        treeGridParentColumns();
+        element.appendChild(CodeCard.createCodeCard(CodeResource.INSTANCE.treeGridParentColumns()).element());
+
         allInOne();
         element.appendChild(CodeCard.createCodeCard(CodeResource.INSTANCE.allInOne()).element());
+
 
         try {
             JsonResource.INSTANCE.generatedJson().getText(new ResourceCallback<TextResource>() {
@@ -150,8 +174,12 @@ public class DataTableViewImpl extends BaseDemoView<HTMLDivElement> implements D
                 @Override
                 public void onSuccess(TextResource resource) {
                     ContactList contactList = ContactList.MAPPER.read(resource.getText());
+                    List<Contact> contacts = contactList.getContacts();
+                    List<Contact> level1 = addFriends(contacts, contacts.subList(0, 50));
+                    List<Contact> level2 = addFriends(contacts, level1);
+                    List<Contact> level3 = addFriends(contacts, level2);
                     contactListParseHandlers.forEach(contactListParseHandler ->
-                            contactListParseHandler.onContactsParsed(contactList.getContacts()));
+                            contactListParseHandler.onContactsParsed(contacts));
                     uiHandlers.stopLoading();
                 }
             });
@@ -162,6 +190,185 @@ public class DataTableViewImpl extends BaseDemoView<HTMLDivElement> implements D
         return element;
     }
 
+    private List<Contact> addFriends(List<Contact> pool, List<Contact> contacts) {
+        List<Contact> result = new ArrayList<>();
+        contacts.forEach(contact -> {
+            int start = new Random().nextInt(50);
+            List<Contact> friends = pool.subList(start, start + new Random().nextInt(5))
+                    .stream()
+                    .map(Contact::new)
+                    .collect(Collectors.toList());
+
+            contacts.forEach(c -> c.setFriends(friends));
+            result.addAll(friends);
+        });
+
+        return result;
+    }
+
+    @SampleMethod
+    private void treeGridFullParentSpan() {
+
+        TableConfig<Contact> tableConfig = new TableConfig<>();
+        tableConfig
+                .addColumn(ColumnConfig.<Contact>create("id", "#")
+                        .textAlign("right")
+                        .asHeader()
+                        .setCellRenderer(cell -> TextNode.of(cell.getTableRow().getRecord().getIndex() + 1 + "")))
+
+                .addColumn(ColumnConfig.<Contact>create("status", "Status")
+                        .textAlign("center")
+                        .setCellRenderer(cell -> {
+                            if (cell.getTableRow().getRecord().isActive()) {
+                                return Style.of(Icons.ALL.check_circle()).setColor(Color.GREEN_DARKEN_3.getHex()).element();
+                            } else {
+                                return Style.of(Icons.ALL.highlight_off()).setColor(Color.RED_DARKEN_3.getHex()).element();
+                            }
+                        }))
+                .addColumn(ColumnConfig.<Contact>create("gender", "Gender")
+                        .setCellRenderer(cell -> ContactUiUtils.getGenderElement(cell.getRecord()))
+                        .textAlign("center"))
+
+                .addColumn(ColumnConfig.<Contact>create("eyeColor", "Eye color")
+                        .setCellRenderer(cell -> ContactUiUtils.getEyeColorElement(cell.getRecord()))
+                        .textAlign("center"))
+
+                .addColumn(ColumnConfig.<Contact>create("balance", "Balance")
+                        .setCellRenderer(cellInfo -> ContactUiUtils.getBalanceElement(cellInfo.getRecord())))
+
+                .addColumn(ColumnConfig.<Contact>create("email", "Email")
+                        .setCellRenderer(cell -> TextNode.of(cell.getTableRow().getRecord().getEmail())))
+
+                .addColumn(ColumnConfig.<Contact>create("phone", "Phone")
+                        .setCellRenderer(cell -> TextNode.of(cell.getTableRow().getRecord().getPhone())))
+
+                .addColumn(ColumnConfig.<Contact>create("badges", "Badges")
+                        .setCellRenderer(cell -> {
+                            if (cell.getTableRow().getRecord().getAge() < 35) {
+                                return Badge.create("Young")
+                                        .setBackground(ColorScheme.GREEN.color()).element();
+                            }
+                            return TextNode.of("");
+                        }))
+                .onUtilityColumn(utilityColumn -> {
+                    utilityColumn.setSortable(true, "id");
+                })
+                .setUtilityColumnTitle("First name")
+                .setMultiSelect(true)
+                .addPlugin(new SortPlugin<>())
+                .addPlugin(new SelectionPlugin<>())
+                .addPlugin(new RecordDetailsPlugin<>(cell -> new ContactDetails(cell).element()))
+                .addPlugin(new RowMarkerPlugin<>(tableCellInfo -> ContactUiUtils.getBalanceColor(tableCellInfo.getRecord())))
+                .addPlugin(new TreeGridPlugin<Contact>((parent, itemsConsumer) -> {
+                    itemsConsumer.accept(Optional.ofNullable(parent.getFriends()));
+                })
+                        .setIndentColumnElementSupplier(tableRow -> Paragraph.create(tableRow.getRecord().getName()).setMarginBottom("0").element())
+                        .setParentRowCellsSupplier((dataTable, tableRow) -> {
+                            HTMLTableCellElement cellElement = DominoElement.of(td())
+                                    .setAttribute("colspan", "8")
+                                    .element();
+                            RowCell<Contact> rowCell =
+                                    new RowCell<>(new CellRenderer.CellInfo<>(tableRow, cellElement), dataTable.getTableConfig().getColumnByName("id"));
+                            return Collections.singletonList(rowCell);
+                        })
+                        .setIndent(60));
+
+        LocalListDataStore<Contact> localListDataStore = new LocalListDataStore<>();
+        DataTable<Contact> table = new DataTable<>(tableConfig, localListDataStore);
+
+        element.appendChild(Card.create("TREE GRID PLUGIN - Full parent span", "Render records in tree style with expand and collapse features")
+                .setCollapsible()
+                .appendChild(new TableStyleActions(table))
+                .appendChild(table)
+                .element());
+
+        contactListParseHandlers.add(contacts -> {
+            localListDataStore.setData(contacts.subList(0, 25));
+            table.load();
+        });
+    }
+
+    @SampleMethod
+    private void treeGridParentColumns() {
+
+        TableConfig<Contact> tableConfig = new TableConfig<>();
+        tableConfig
+                .addColumn(ColumnConfig.<Contact>create("id", "#")
+                        .textAlign("right")
+                        .asHeader()
+                        .setCellRenderer(cell -> TextNode.of(cell.getTableRow().getRecord().getIndex() + 1 + "")))
+
+                .addColumn(ColumnConfig.<Contact>create("status", "Status")
+                        .textAlign("center")
+                        .setCellRenderer(cell -> {
+                            if (cell.getTableRow().getRecord().isActive()) {
+                                return Style.of(Icons.ALL.check_circle()).setColor(Color.GREEN_DARKEN_3.getHex()).element();
+                            } else {
+                                return Style.of(Icons.ALL.highlight_off()).setColor(Color.RED_DARKEN_3.getHex()).element();
+                            }
+                        }))
+                .addColumn(ColumnConfig.<Contact>create("gender", "Gender")
+                        .setCellRenderer(cell -> ContactUiUtils.getGenderElement(cell.getRecord()))
+                        .textAlign("center"))
+
+                .addColumn(ColumnConfig.<Contact>create("eyeColor", "Eye color")
+                        .setCellRenderer(cell -> ContactUiUtils.getEyeColorElement(cell.getRecord()))
+                        .textAlign("center"))
+
+                .addColumn(ColumnConfig.<Contact>create("balance", "Balance")
+                        .setCellRenderer(cellInfo -> ContactUiUtils.getBalanceElement(cellInfo.getRecord())))
+
+                .addColumn(ColumnConfig.<Contact>create("email", "Email")
+                        .setCellRenderer(cell -> TextNode.of(cell.getTableRow().getRecord().getEmail())))
+
+                .addColumn(ColumnConfig.<Contact>create("phone", "Phone")
+                        .setCellRenderer(cell -> TextNode.of(cell.getTableRow().getRecord().getPhone())))
+
+                .addColumn(ColumnConfig.<Contact>create("badges", "Badges")
+                        .setCellRenderer(cell -> {
+                            if (cell.getTableRow().getRecord().getAge() < 35) {
+                                return Badge.create("Young")
+                                        .setBackground(ColorScheme.GREEN.color()).element();
+                            }
+                            return TextNode.of("");
+                        }))
+                .onUtilityColumn(utilityColumn -> {
+                    utilityColumn.setSortable(true, "id");
+                })
+                .setUtilityColumnTitle("First name")
+                .setMultiSelect(true)
+                .addPlugin(new SortPlugin<>())
+                .addPlugin(new SelectionPlugin<>())
+                .addPlugin(new RecordDetailsPlugin<>(cell -> new ContactDetails(cell).element()))
+                .addPlugin(new RowMarkerPlugin<>(tableCellInfo -> ContactUiUtils.getBalanceColor(tableCellInfo.getRecord())))
+                .addPlugin(new TreeGridPlugin<Contact>((parent, itemsConsumer) -> {
+                    itemsConsumer.accept(Optional.ofNullable(parent.getFriends()));
+                })
+                        .setIndentColumnElementSupplier(tableRow -> Paragraph.create(tableRow.getRecord().getName()).setMarginBottom("0").element())
+
+                        .setIndent(60));
+
+        LocalListDataStore<Contact> localListDataStore = new LocalListDataStore<>();
+        DataTable<Contact> table = new DataTable<>(tableConfig, localListDataStore);
+
+        element.appendChild(Card.create("TREE GRID PLUGIN - Parent with columns", "Render records in tree style with expand and collapse features")
+                .setCollapsible()
+                .appendChild(new TableStyleActions(table))
+                .appendChild(table)
+                .element());
+
+        contactListParseHandlers.add(contacts -> {
+            localListDataStore.setData(contacts.subList(0, 25));
+            table.load();
+        });
+    }
+
+    private void sortChildren(TreeGridSample item, Comparator<TreeGridSample> comparator) {
+        item.getItems().sort(comparator);
+        for (TreeGridSample itemItem : item.getItems()) {
+            sortChildren(itemItem, comparator);
+        }
+    }
 
     @SampleMethod
     private void groupingTable() {
@@ -257,7 +464,6 @@ public class DataTableViewImpl extends BaseDemoView<HTMLDivElement> implements D
                 .addColumn(ColumnConfig.<Contact>create("firstName", "First name")
                         .setCellRenderer(cell -> TextNode.of(cell.getTableRow().getRecord().getName())))
 
-
                 .addColumn(ColumnConfig.<Contact>create("gender", "Gender")
                         .setCellRenderer(cell -> ContactUiUtils.getGenderElement(cell.getRecord()))
                         .textAlign("center"))
@@ -315,7 +521,7 @@ public class DataTableViewImpl extends BaseDemoView<HTMLDivElement> implements D
                                 .clickable()
                                 .setTooltip("Save")
                                 .addClickListener(evt -> {
-                                    if(cell.getTableRow().validate().isValid()) {
+                                    if (cell.getTableRow().validate().isValid()) {
                                         cell.getTableRow().save();
                                     }
                                 })
@@ -542,6 +748,10 @@ public class DataTableViewImpl extends BaseDemoView<HTMLDivElement> implements D
 
     private List<Contact> subList(List<Contact> contacts) {
         return subList(contacts, 0, 15);
+    }
+
+    private List<Contact> subList(List<Contact> contacts, int size) {
+        return subList(contacts, 0, size);
     }
 
     private List<Contact> subList(List<Contact> contacts, int from, int to) {
